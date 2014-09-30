b45fd1cdf9356bc92abc7a43d6045843c5f4dfae
Christopher Johnson <christopher.johnson@wikimedia.de>
2014-09-30 10:48:09 +0200

    Create init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+class phabricator (
+  $base_dir    = $phabricator::params::base_dir,
+  $environment = $phabricator::params::environment,
+) inherits phabricator::params {
+
+  validate_absolute_path($base_dir)
+
+  validate_string($environment)
+  if ! ($environment in ['development', 'production']) {
+    fail('environment parameter must be "development" or "production"')
+  }
+
+  class { 'phabricator::config':
+    environment => $environment,
+  }
+}
